b4ccf63083f3aad0f68c3ab64037bcad6961fc10
Robert Jin <robert.jin@vgbio.local>
2017-01-22 19:12:45 -0600

    Room Obj

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ server.listen(2000);
 console.log("Listening");
 
 var SOCKET_LIST = {};
-var ACTIVE_GAMES = {};
+var ACTIVE_ROOMS = {};
+
 
 var io = require('socket.io')(server, {});
 io.sockets.on('connection', function(socket){
-	socket.id = Math.random();
 
 	SOCKET_LIST[socket.id] = socket;
 	console.log(socket.id);
@@ -26,13 +26,28 @@ io.sockets.on('connection', function(socket){
 	socket.on('createNewRoom', function(data){
 		console.log(data.name + " has created a new room");
 		var newRoomID = makeRoomID();
-		ACTIVE_GAMES[newRoomID] = {data.name};
+		var newPlayer = data.name;
+		var newRoom = new Room(newRoomID);
+		newRoom.test();
+		newRoom.addPlayer(data.name);
+		console.log(newRoom);
+
+		ACTIVE_ROOMS[newRoomID] = newRoom;
+		socket.emit('RoomResponse', ACTIVE_ROOMS[newRoomID]);
 	})
 
 	socket.on('enterRoom', function(data){
-	
-		console.log(data.name + " has just joined the room " + data.rID);
-
+		if (data.rID in ACTIVE_ROOMS){
+			//if data.name in ACTIVE_ROOMS[data.RID].players
+			console.log(data.name + " has just joined the room " + data.rID);
+			var game = ACTIVE_ROOMS[data.rID];
+			ACTIVE_ROOMS[data.rID].addPlayer(data.name);
+			console.log(ACTIVE_ROOMS[data.rID]);
+			io.sockets.emit('RoomResponse', ACTIVE_ROOMS[data.rID])
+		}
+		else{
+			socket.emit('error', {msg: "Room does not exist"});
+		}
 	})
 
 	socket.on('disconnect', function(){
@@ -41,7 +56,34 @@ io.sockets.on('connection', function(socket){
 
 })
 
-var makeRoomID(){
+/* Room should have information about roomID for unique identification, state (whether it's new & waiting for players, ready to play, ongoing, or completed)
+
+*/
+function Room(roomID){
+	this.roomID = roomID;
+	this.state = 0;
+	this.lastPerson = null;
+	this.players = [];
+	this.addPlayer = function(Name){
+		this.players.push({socketID: socket.id, personName: Name, hiddenName: Name});
+	}
+	this.test = function(){
+		console.log("Testing");
+	}
+	this.updateState = function(){
+		if (this.state < 3){
+			this.state += 1;
+		}
+		else{
+			console.log("Game is completed")
+			this.state = 0;
+		}
+		console.log("updated state:", this.state);
+	}
+
+}
+
+function makeRoomID(){
 	var text = "";
 	var possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	for (var i = 0; i<5; i++){
---
+++ client/js/main.js
@@ -0,0 +1,50 @@
+// var io = require('socket.io-client');
+console.log("Hello");
+var socket = io();
+
+var roomID = document.getElementById('roomID');
+var playerName = document.getElementById('playerName');
+var enterRoomButton = document.getElementById('enterRoomButton')
+var newRoomButton = document.getElementById('createNewRoomButton')
+
+newRoomButton.onclick = function(){
+	socket.emit('createNewRoom', {name: playerName.value});
+	console.log("Creating new Room..")
+}
+
+enterRoomButton.onclick = function(){
+	socket.emit('enterRoom', {name: playerName.value, rID: roomID.value});
+	console.log("Entering room..");
+
+}
+
+socket.on('RoomResponse', function(gameObj){
+	console.log(gameObj);
+	update(gameObj);
+});
+
+
+//////////////////////////////////
+
+//Update DOM elements
+function update(gameObj){
+	var roomID = document.getElementById('roomIDinfo');
+	// for i in gameObj.players{
+	// 	roomID.innerHTML += i.personName;
+	// }
+	roomID.innerHTML = "RoomID: ";
+	var players = gameObj.players;
+	for (var i = 0; i < players.length; i++){
+		console.log(players[i].personName);
+		
+		roomID.innerHTML += players[i].personName + " ";
+	}
+
+	// console.log(players);
+	
+	// roomID.innerHTML += "boing";
+	
+	//console.log(roomID);
+
+
+}
